57f361a2fdd50ddc95543f13294e876aedbd4cce
sayland1-prog <sayland1@gmail.com>
2025-11-03 17:09:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 function sayHello() {
   alert("こんにちは！JavaScriptが動きました 🎉");
 }
+function showMessage(event) {
+  event.preventDefault(); // ページがリロードされるのを防ぐ
+
+  const name = document.getElementById("name").value;
+  const message = document.getElementById("message").value;
+
+  const output = document.getElementById("output");
+  output.innerHTML = `${name} さん、メッセージありがとうございます！<br>"${message}" を受け取りました 😊`;
+}
